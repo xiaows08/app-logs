3b2955c7b9c5f2c65e8b2fda0dad643df4698ace
XIAOWS <xiaows08@163.com>
2019-01-04 16:07:33 +0800

    MyMapper

--- app-logs-web/src/main/java/cn/xiaows/app/util/MyMapper.java
+++ app-logs-web/src/main/java/cn/xiaows/app/util/MyMapper.java
@@ -29,10 +29,11 @@ import tk.mybatis.mapper.common.MySqlMapper;
 
 /**
  * 继承自己的MyMapper
+ * 特别注意，该接口不能被扫描到，否则会出错
  *
  * @author liuzh
  * @since 2015-09-06 21:53
  */
 public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
-    //FIXME 特别注意，该接口不能被扫描到，否则会出错
+
 }
